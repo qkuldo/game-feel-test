0dfff56a97b07b1e5e926e50c08b8fcf989a7f38
qkuldo <teoa52269@gmail.com>
2024-10-07 16:59:58 +0800

    Added movement code & particle system

--- classes.py
+++ classes.py
@@ -4,5 +4,23 @@ class character:
 		self.pos = pos
 		self.draw_color = draw_color
 		self.character = pygame.Rect(self.pos[0],self.pos[1],50,100)
+	def update(self):
+		self.character.x = self.pos[0]
+		self.character.y = self.pos[1]
 	def draw(self,screen):
-        pygame.draw.rect(screen,self.draw_color,self.character)
+		pygame.draw.rect(screen,self.draw_color,self.character)
+class particle:
+	def __init__(self,pos,dx,dy,life=300):
+		self.pos = pos
+		self.dx = dx
+		self.dy = dy
+		self.life = life
+	def update(self,clock):
+		self.pos[0] += self.dx
+		self.pos[1] += self.dy
+		self.life -= clock.get_time()
+		if (self.life <= 0):
+			return 1
+	def draw(self,screen):
+		rect = pygame.Rect(self.pos[0],self.pos[1],30,30)
+		pygame.draw.rect(screen,(255,255,255),rect)
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import pygame
 import classes
-
+import sys
+import random
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 player = classes.character([300,300],(255,95,95))
+clock = pygame.time.Clock()
+movement_particles = []
+while True:
+	screen.fill((95,95,100))
+	for event in pygame.event.get():
+		if (event.type == pygame.QUIT):
+			pygame.quit()
+			sys.exit()
+	keys = pygame.key.get_pressed()
+	if (keys[pygame.K_UP]):
+		player.pos[1] -= 5
+		for i in range(5):
+			movement_particles.append(classes.particle([player.character.center[0],player.character.bottom],random.randint(-5,5),5,50))
+	elif (keys[pygame.K_DOWN]):
+		player.pos[1] += 5
+		for i in range(5):
+			movement_particles.append(classes.particle([player.character.center[0],player.character.top-20],random.randint(-5,5),-5,50))
+	if (keys[pygame.K_LEFT]):
+		player.pos[0] -= 5
+		for i in range(5):
+			movement_particles.append(classes.particle([player.character.bottomright[0],player.character.center[1]],5,random.randint(-5,5),50))
+	elif (keys[pygame.K_RIGHT]):
+		player.pos[0] += 5
+		for i in range(5):
+			movement_particles.append(classes.particle([player.character.bottomleft[0]-20,player.character.center[1]],5,random.randint(-5,5),50))
+	player.update()
+	player.draw(screen)
+	for particle in movement_particles:
+		status = particle.update(clock)
+		if (status == 1):
+			movement_particles.remove(particle)
+		else:
+			particle.draw(screen)
+	pygame.display.update()
+	clock.tick(60)
